399ae938d59bd48de4b625f27e6a7cc998e2e5a9
zheng zhang <zzheng@prevert>
2018-09-10 18:19:29 +0200

    debug

--- matrix2vec_server.py
+++ matrix2vec_server.py
@@ -2,8 +2,8 @@ import matrix_enhancer as me
 
 # Generate cooccurrence_matrix and tokens of different window sizes from gow/output/intermediate data/graph/ to
 # matrix2vec/input/
-gow_folder_path = '~/Code/gow/output/intermediate data/'
-for i in (2, 11):
+gow_folder_path = '../gow/output/intermediate data/'
+for i in range(2, 11):
     encoded_edges_count_file_path = gow_folder_path + 'graph/encoded_edges_count_window_size_' \
                                     + str(i) + '_undirected.txt'
     me.MatrixEnhancer.from_encoded_edges_count_file_path(encoded_edges_count_file_path=encoded_edges_count_file_path,
