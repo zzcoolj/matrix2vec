288cdec041a20de9a56c7fd2a2028a325e6e50b3
zzcoolj <zheng.zhang@limsi.fr>
2018-09-13 19:10:39 +0200

    matrixMixer test

--- matrix2vec_server.py
+++ matrix2vec_server.py
@@ -67,7 +67,8 @@ Get matrix from ppmi/ and first_order/ and generate ppmi+firstOrder/ intermediat
 
 m = me.MatrixMixer.from_storage(base_matrix_path='output/intermediate_data/ppmi/ppmi_w5.npy',
                                 ingredient_matrix_path='output/intermediate_data/first_order/firstOrder_w5.npy')
-for k, mixed_matrix in m.grid_search_k_yielder(ks=[1,2], output_folder='output/intermediate_data/ppmi+firstOrder/'):
+for k, mixed_matrix in m.grid_search_k_yielder(ks=[0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100],
+                                               output_folder='output/intermediate_data/ppmi+firstOrder/'):
     for dimension in [500]:
         vectors = me.MatrixDimensionReducer.truncated_svd(mixed_matrix, dimension)
         me.MatrixDimensionReducer.save_enhanced_matrix(vectors,
